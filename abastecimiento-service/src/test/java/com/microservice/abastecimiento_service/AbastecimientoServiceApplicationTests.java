627ccd12b71cfc8215d8f52d877bdc510b2143f6
Neru54 <pabl.cabezas@duocuc.cl>
2026-06-20 15:53:40 -0400

    se agrega confirguracion de service test en abastecimiento y producto.

--- abastecimiento-service/src/test/java/com/microservice/abastecimiento_service/AbastecimientoServiceApplicationTests.java
+++ abastecimiento-service/src/test/java/com/microservice/abastecimiento_service/AbastecimientoServiceApplicationTests.java
@@ -2,9 +2,30 @@ package com.microservice.abastecimiento_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+import com.microservice.abastecimiento_service.feignclient.InventarioClient;
+import com.microservice.abastecimiento_service.feignclient.ProductoClient;
+import com.microservice.abastecimiento_service.feignclient.ProveedorClient;
+import com.microservice.abastecimiento_service.repository.OrdenCompraRepository;
+
+@SpringBootTest(properties = {
+	"spring.cloud.config.enabled=false",
+	"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration,org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration"
+})
 class AbastecimientoServiceApplicationTests {
+
+	@MockitoBean
+	private OrdenCompraRepository ordenCompraRepository;
+
+	@MockitoBean
+	private InventarioClient inventarioClient;
+
+	@MockitoBean
+	private ProductoClient productoClient;
+
+	@MockitoBean
+	private ProveedorClient proveedorClient;
 
 	@Test
 	void contextLoads() {
--- producto-service/src/test/java/com/microservice/producto_service/ProductoServiceApplicationTests.java
+++ producto-service/src/test/java/com/microservice/producto_service/ProductoServiceApplicationTests.java
@@ -2,9 +2,22 @@ package com.microservice.producto_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+import com.microservice.producto_service.feignClient.InventarioClient;
+import com.microservice.producto_service.repository.ProductoRepository;
+
+@SpringBootTest(properties = {
+	"spring.cloud.config.enabled=false",
+	"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration,org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration"
+})
 class ProductoServiceApplicationTests {
+
+	@MockitoBean
+	private ProductoRepository productoRepository;
+
+	@MockitoBean
+	private InventarioClient inventarioClient;
 
 	@Test
 	void contextLoads() {
